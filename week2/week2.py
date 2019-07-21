69880400bcb2cd201a6d23f3b3f528c93e5149a4
Daniel-Ivan-Flores-Guerrero <daniel.ivan.flores.guerrero@ibm.com>
2019-07-20 21:34:12 -0400

    init

---
+++ week2/week2.py
@@ -0,0 +1,19 @@
+""" 
+1. Write a program that reads two lists
+	 of numbers (4 items minimum) and merge 
+   them bysorting them out ignoring duplicates
+"""
+list1 = ["a","b","c","d","e"]
+list2 = ["a","b","c","d","e"]
+list2 = list2[::-1]
+print(list2)
+
+
+
+
+""" 
+2. Improve the previous code by ignoring the ones  
+	 that could be written as a linear combinationof
+	 any other two numbers (13=2*5+1*3), so if 3 and
+	 5 are there, you should drop 13 if seen.
+"""
